32556d4f073aa318b11774fcfe635b7ad5e8ec9c
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-08-22 21:43:24 +0800

    added projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,25 +5,55 @@ import {
     CardTitle,
     CardSubtitle, 
     CardText, 
-    CardLink
+    CardLink, 
+    CardImg, 
+    Button, 
+    CardGroup
     } from 'reactstrap';
 
 class Projects extends React.Component {
     render() {
         return(
             <>
-                <Card>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                    </CardBody>
-                    <a href="https://nextagram-done.herokuapp.com" target="_blank"><img src="https://is1-ssl.mzstatic.com/image/thumb/Purple127/v4/c5/25/8c/c5258ce9-90dc-b62a-c9af-9d6fd5f9b59d/source/512x512bb.jpg" alt="Card image cap" max-width='33%' height='300px'/></a>
-                    <CardBody>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                        <CardLink href="#">Card Link</CardLink>
-                        <CardLink href="#">Another Link</CardLink>
-                    </CardBody>
-                </Card>
+                <h1 className="text-center"><strong><em>My Projects</em></strong></h1>
+                
+                <CardGroup className="projects" style={{width:'550px'}}>
+                    <a href="https://sugar-combat.herokuapp.com/" style={{textDecoration:'none',color:'black'}}>
+                        <Card>
+                            <img src="https://nextagram-flask.s3.amazonaws.com/Sugar+combat-pic.png" alt="sugar combat" width='550px' height='300px'/>
+                            <CardBody>
+                                <CardTitle><strong>Sugar Combat</strong></CardTitle>
+                                <CardText className="flex-wrap">This "Sugar Combat" app was built using Python and Flask. This is my first final project which challenged me to learn Python, Flask, Javascript, HTML and SQL. </CardText>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                    <br />
+
+                    <a href="https://nextagram-done.herokuapp.com" style={{textDecoration:'none',color:'black'}}>
+                        <Card>
+                            <img src="https://nextagram-flask.s3.amazonaws.com/Nextagram-pic.png" alt="nextagram" width='550px' height='300px'/>
+                            <CardBody>
+                                <CardTitle><strong>Nextagram</strong></CardTitle>
+                                <CardText className="flex-wrap">This "Nextagram" app was built using Python and Flask. This is my first full stack project which challenged me to learn Python, Flask and SQL. </CardText>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                    <br />
+
+                    <a href="https://awesome-davinci-55503e.netlify.com/" style={{textDecoration:'none',color:'black'}}>
+                        <Card>
+                            <CardImg top width="100%" src="https://nextagram-flask.s3.amazonaws.com/Tic+Tac+Toe-pic.png" alt="tic tac toe" width='300px' height='300px' />
+                            <CardBody>
+                                <CardTitle><strong>Tic Tac Toe</strong></CardTitle>
+                                <CardText>This project uses HTML, CSS and JavaScript. Players click on letters to try guessing the hidden word.</CardText>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                </CardGroup>
+                
             </>
         )
     }
